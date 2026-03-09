b4c5517a81578615e6b3cc388e92f84532cf5db6
Shinor Spellz <Toziz@yahoo.com>
2026-03-09 04:24:50 +0000

    Initial Commit

---
+++ Prelura-swift/Prelura_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Prelura_swiftApp.swift
+//  Prelura-swift
+//
+//  Created by User on 09/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct Prelura_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
